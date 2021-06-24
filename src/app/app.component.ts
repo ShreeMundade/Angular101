4ee112674fa2e4050042fb3cb4c8197f14cc068e
ShreeMundade <shreemundade@gmail.com>
2021-06-24 15:27:55 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'puppers-app';
+  title = 'Just Puppers!';
+  breed:string='labrador' ;
+  findDog(){
+    console.log(this.breed);
+  }
 }
